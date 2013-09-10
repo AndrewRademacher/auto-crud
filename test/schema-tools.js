5801813ba911f83a4189d982f2d9fedf74a0c526
Andrew Rademacher <andrewrademacher@gmail.com>
2013-09-10 16:48:06 -0500

    Added ability to create filtered schemas in schema-tools.

--- test/schema-tools.js
+++ test/schema-tools.js
@@ -1,6 +1,8 @@
 var _ = require('underscore'),
     assert = require('assert'),
-    getMongoProjection = require('../lib/schema-tools').getMongoProjection;
+    getMongoProjection = require('../lib/schema-tools').getMongoProjection,
+    getPostSchema = require('../lib/schema-tools').getPostSchema,
+    getPutSchema = require('../lib/schema-tools').getPutSchema;
 
 var sampleSchema = {
     type: 'object',
@@ -12,8 +14,8 @@ var sampleSchema = {
         },
         password: {
             type: 'string',
-            required: true,
-            hidden: true
+            required_post: true,
+            hidden: true,
         },
         stripe: {
             type: 'object',
@@ -25,10 +27,14 @@ var sampleSchema = {
                 },
                 stripeKey: {
                     type: 'string',
-                    required: true,
+                    required_post: true,
                     hidden: true
                 }
             }
+        },
+        updateTime: {
+            type: 'string',
+            required_put: true
         },
         subUsers: {
             type: 'array',
@@ -42,7 +48,7 @@ var sampleSchema = {
                     },
                     password: {
                         type: 'string',
-                        required: true,
+                        required_post: true,
                         hidden: true
                     }
                 }
@@ -59,5 +65,56 @@ describe('Schema Manipulation (Library)', function() {
             'stripe.stripeKey': 0,
             'subUsers.password': 0
         }));
+    });
+
+    function removeHiddenTags(schema) {
+        delete schema.properties.password.hidden;
+        delete schema.properties.stripe.properties.stripeKey.hidden;
+        delete schema.properties.subUsers.items.properties.password.hidden;
+    }
+
+    function removeRequiredByTags(schema) {
+        delete schema.properties.password.required_post;
+        delete schema.properties.stripe.properties.stripeKey.required_post;
+        delete schema.properties.subUsers.items.properties.password.required_post;
+
+        delete schema.properties.updateTime.required_put;
+    }
+
+    var sampleSchemaCache = JSON.stringify(sampleSchema);
+
+    describe('Post Schema (Variant)', function() {
+        it('should replace requirePost tags with the require tag and remove requirePut tags', function() {
+            var variant = JSON.parse(JSON.stringify(sampleSchema));
+
+            removeHiddenTags(variant);
+            removeRequiredByTags(variant);
+
+            variant.properties.password.required = true;
+            variant.properties.stripe.properties.stripeKey.required = true;
+            variant.properties.subUsers.items.properties.password.required = true;
+
+            assert(_.isEqual(variant, getPostSchema(sampleSchema)));
+        });
+
+        it('should not allow varient schemas to alter original', function() {
+            assert(_.isEqual(JSON.parse(sampleSchemaCache), sampleSchema));
+        });
+    });
+
+    describe('Put Schema (Variant)', function() {
+        it('should replace requirePut tags with the require tag, and remove requirePost tags', function() {
+            var variant = JSON.parse(JSON.stringify(sampleSchema));
+
+            removeHiddenTags(variant);
+            removeRequiredByTags(variant);
+
+            variant.properties.updateTime.required = true;
+            assert(_.isEqual(variant, getPutSchema(sampleSchema)));
+        });
+
+        it('should not allow varient schemas to alter original', function() {
+            assert(_.isEqual(JSON.parse(sampleSchemaCache), sampleSchema));
+        });
     });
 });
